af13b51a5995c72de1a14f8c09f1856148225a81
Leonardo Gallo <lgallo@ciencias.unam.mx>
2019-02-14 18:59:24 -0600

    agregando uso rutas absolutas y relativas

---
+++ practicas/src/exec.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#Despliega mensaje
+echo "El contenido de esta carpeta es:"
+ls
+echo "Los detalles y archivos ocultos son:"
+ls -al
+#En el siguiente comando uso una ruta absoluta
+echo "Despliega el contenido de la carpeta raíz"
+ls /
+#Uso una ruta relativa en el siguiente comando
+touch ejercicios/resources/texto.txt
