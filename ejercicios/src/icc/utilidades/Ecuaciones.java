78a9e27eda9b045bca03f9261a6edade08fb0990
Leonardo Gallo <lgallo@ciencias.unam.mx>
2019-03-21 18:20:32 -0600

    ejercicio switch

---
+++ ejercicios/src/icc/utilidades/Ecuaciones.java
@@ -0,0 +1,22 @@
+package icc.utilidades;
+
+/**
+ * Ejercicio para uso de la estructura de contro switch
+ * Realizamos una operación dependiendo del rango en el que se
+ * encuentre una variable x, valor que ingresa el usuario.
+ *
+ * @author Leonardo Gallo
+ * @version 20/03/2019
+ */
+public class Ecuaciones {
+
+    /**
+     * Método principal.
+     * 
+     * @param args argumentos del método main
+     */
+    public static void main(String[] args) {
+	
+    }
+
+}
