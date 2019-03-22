90a417fcd632ac3400415d607922165555a7a590
Leonardo Gallo <lgallo@ciencias.unam.mx>
2019-03-21 18:26:52 -0600

    ejercicio for, while y do-while

---
+++ ejercicios/src/icc/utilidades/Perfectos.java
@@ -0,0 +1,24 @@
+package icc.utilidades;
+
+import java.util.Scanner;
+
+/**
+ * Ejercicio para diferenciar el uso de while, for y do-while
+ * Descubrir si un número es perfecto o no, crear una clase
+ * con la solución usando una estructura cíclica distinta.
+ *
+ * @author Leonardo Gallo
+ * @version 13/03/2019
+ */
+public class Perfectos {
+    /** Variables y objetos de clase */
+
+    /**
+     * Método principal
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+	//Aquí va tu solución...
+    }
+}
