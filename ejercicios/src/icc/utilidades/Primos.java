67972ec09b2db9713853cdc2de1682a7aceeae64
Leonardo Gallo <lgallo@ciencias.unam.mx>
2019-03-21 18:29:14 -0600

    ejercicio diagramas para números primos y perfectos

---
+++ ejercicios/src/icc/utilidades/Primos.java
@@ -0,0 +1,22 @@
+package icc.utilidades;
+
+import java.util.Scanner;
+
+/**
+ *
+ *
+ * @author Leonardo Gallo
+ * @version 07/03/2019
+ */
+public class Primos {
+    /** Variables y objetos de clase */
+
+    /**
+     * Método principal
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+	//Aquí va tu implementación
+    }
+}
