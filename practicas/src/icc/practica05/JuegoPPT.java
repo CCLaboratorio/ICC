3495a364115f8f123ab2183104a8d63ce275d875
Leonardo Gallo <lgallo@ciencias.unam.mx>
2019-04-29 00:02:45 -0500

    plantillas piedra papel o tijera

---
+++ practicas/src/icc/practica05/JuegoPPT.java
@@ -0,0 +1,66 @@
+package icc.practica05;
+
+import java.util.Scanner;
+
+/**
+ *
+ *
+ * @author Leonardo Gallo
+ * @version 24/10/2018
+ */
+public class JuegoPPT {
+
+    /** Variables de clase */
+    private String jugador;
+    private int eleccionMaquina;
+    private char eleccionHumano;
+    
+    /**
+     *
+     * Consructor por defecto
+     */
+    public JuegoPPT() {
+	//aquí va tu implementación
+    }
+
+    /**
+     * COMÉNTAME...
+     *
+     */
+    public void empezarJuego(Scanner sc) {
+	//aquí va tu implementación
+    }
+
+    /**
+     * COMÉNTAME...
+     *
+     */
+    public void escogerPPT() {
+	//aquí va tu implementación
+    }
+
+    /**
+     * COMÉNTAME...
+     *
+     */
+    public char pedirEleccion(Scanner sc) {
+	//aquí va tu implementación
+	return 'a';
+    }
+    
+    /**
+     * COMÉNTAME...
+     *
+     */
+    public void determinarGanador() {
+	//aquí va tu implementación
+    }
+
+    /**
+     * COMÉNTAME...
+     *
+     */
+    private void informar(char yo, char yoGano, char tuGanas) {
+	//aquí va tu implementación
+    }
+}
---
+++ practicas/src/icc/practica05/UsoJuego.java
@@ -0,0 +1,19 @@
+package icc.practica05;
+
+/**
+ *
+ *
+ * @author Leonardo Gallo
+ * @version 24/10/2018
+ */
+public class UsoJuego {
+
+    /**
+     *
+     * Metodo main
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
