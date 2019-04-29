7ef8735d26f88f6055a56b1a0b2ba6eb4542070e
Leonardo Gallo <lgallo@ciencias.unam.mx>
2019-04-29 00:04:47 -0500

    ejercicio sobre cadenas

---
+++ ejercicios/src/icc/utilidades/ProcesadorDeCadenas.java
@@ -0,0 +1,52 @@
+package icc.practica06;
+
+/**
+ *
+ *
+ * @author Leonardo Gallo
+ * @version 28/04/2019
+ */
+public class ProcesadorDeCadenas {
+
+    /**
+     * Constructor por defecto
+     *
+     */
+    public ProcesadorDeCadenas() {
+
+    }
+
+    /**
+     * COMÉNTAME...
+     * 
+     */
+    public String generaCadena() {
+	return "";
+    }
+
+    /**
+     * COMÉNTAME...
+     * 
+     */
+    public String generaCadena(int longitud) {
+	return "";
+    }
+
+    /**
+     * COMÉNTAME...
+     * 
+     */
+    public String extraeSubcadena(int inicio, int fin) {
+	return "";
+    }
+
+    
+    /**
+     * COMÉNTAME...
+     * 
+     */
+    public String reemplazaCadena(String patron, String reemplazo) {
+	return "";
+    }
+
+}
